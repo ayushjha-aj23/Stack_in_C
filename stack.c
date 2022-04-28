f3487cfe437489916d7d7674d3bb9c4a049b4ebc
ayushjha-aj23 <60493749+ayushjha-aj23@users.noreply.github.com>
2022-04-28 16:06:46 +0530

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <conio.h>
+#define SIZE 3
+
+
+void push (int stack[], int *Top, int item)
+{
+  if (*Top == SIZE - 1)
+    printf ("Stack Overflow \n");
+  else
+    {
+      *Top = *Top + 1;
+      stack[*Top] = item;
+    }
+
+}
+
+void pop (int stack[], int Top)
+{
+  int item;
+
+  if (Top == -1)
+    printf ("Stack Underflow \n");
+  else
+    {
+      item = stack[Top];
+      printf ("Poped Item is %d \n", item);
+      Top = Top - 1;
+    }
+}
+
+void peek (int stack[], int Top)
+{
+  printf ("Stack Top is %d \n", stack[Top]);
+}
+
+int main ()
+{
+  int choice, item;
+  int STACK[SIZE];
+  int Top = -1;
+
+  while (1)
+    {
+
+      printf ("Press 1: For Push Operation \n");
+      printf ("Press 2: For Pop Operation \n");
+      printf ("Press 3: For Peek Operation \n");
+      printf ("Press any other character to exit \n");
+
+      printf ("Enter a choice \n");
+      scanf ("%d", &choice);
+
+      switch (choice)
+	    {
+    	case 1:
+    	  {
+    	    printf ("Enter an item to be pushed into the stack ");
+    	    scanf ("%d", &item);
+    	    push (STACK, &Top, item);
+    	    break;
+    	  }
+    	case 2:
+    	  pop (STACK, Top);
+    	  break;
+    	case 3:
+    	  peek (STACK, Top);
+    	  break;
+    	default:
+    	  exit (0);
+    
+    	}
+    }
+  return 0;
+}
